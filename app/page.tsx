0fc4f576dede817ac45542322de18140f2a94745
Salik <iamsalikjamal@gmail.com>
2024-01-22 12:56:15 +0500

    Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
     return (
-        <h1 className="text-6xl text-center font-bold mt-5">Home</h1>
+        <h1 className="text-6xl text-center font-bold mt-5">
+            Home
+        </h1>
     )
 }
