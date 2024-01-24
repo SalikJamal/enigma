0e384bd8fdf79fdb8d67a7cbe98bf55e681372fd
Salik <iamsalikjamal@gmail.com>
2024-01-24 11:27:33 +0500

    Mobile sidebar done

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -2,16 +2,28 @@
 
 import { Menu } from "lucide-react"
 import { Button } from "@/components/ui/button"
+import { Sheet, SheetClose, SheetContent, SheetTrigger } from "@/components/ui/sheet"
+import Sidebar from "@/components/sidebar"
 
 
 export default function MobileSidebar() {
     return (
-        <Button
-            className="md:hidden" 
-            variant="ghost" 
-            size="icon"
-        >
-            <Menu size={24} />
-        </Button>
+        <Sheet>
+            <SheetTrigger asChild>
+                <Button
+                    className="md:hidden" 
+                    variant="ghost" 
+                    size="icon"
+                >
+                    <Menu size={24} />
+                </Button>
+            </SheetTrigger>
+            <SheetContent
+                className="w-[300px] p-0 border-r-0"
+                side="left"
+            >
+                <Sidebar />   
+            </SheetContent>
+        </Sheet>
     )
 }
