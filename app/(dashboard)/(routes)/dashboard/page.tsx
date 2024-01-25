9b08eaeae641a4e0c01d24978c4117f076e54499
Salik <iamsalikjamal@gmail.com>
2024-01-25 11:24:40 +0500

    Factorizing

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,16 +1,58 @@
 'use client'
 
+// import { routes } from "@/lib/data" WHY DO THESE ROUTES NOT WORK
 import { Card } from "@/components/ui/card"
 import { cn } from "@/lib/utils"
-import { ArrowRight } from "lucide-react"
+import { ArrowRight, 
+    CodeIcon, 
+    ImageIcon, 
+    MessageSquare, 
+    MusicIcon, 
+    VideoIcon } from "lucide-react"
 import { useRouter } from "next/navigation"
-import { routes } from "@/lib/data"
+
+const tools = [
+    {
+        label: "Conversation",
+        icon: MessageSquare,
+        color: "text-violet-500",
+        bgColor: "bg-violet-500/10",
+        href: "/conversation"
+    },
+    {
+        label: "Image Generation",
+        icon: ImageIcon,
+        color: "text-pink-700",
+        bgColor: "bg-pink-700/10",
+        href: "/image"
+    },
+    {
+        label: "Video Generation",
+        icon: VideoIcon,
+        color: "text-orange-700",
+        bgColor: "bg-orange-700/10",
+        href: "/video"
+    },
+    {
+        label: "Music Generation",
+        icon: MusicIcon,
+        color: "text-emerald-500",
+        bgColor: "bg-emerald-500/10",
+        href: "/music"
+    },
+    {
+        label: "Code Generation",
+        icon: CodeIcon,
+        color: "text-green-700",
+        bgColor: "bg-green-700/10",
+        href: "/code"
+    }
+]
 
 
 export default function DashboardPage() {
 
     const router = useRouter()
-    const toolsOnlyRoutes = routes.slice(1, 6)
 
     return (
         <div>
@@ -23,7 +65,7 @@ export default function DashboardPage() {
                 </p>
             </div>
             <div className="px-4 md:px-20 lg:px-32 space-y-4">
-                {toolsOnlyRoutes.map((tool) => (
+                {tools.map((tool) => (
                     <Card
                         className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer" 
                         key={tool.href}
